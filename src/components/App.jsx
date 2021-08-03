4ab30a8ae630cf178f3807f8cef6bbf9df4edcf5
SterlingHi <sterlinghisey@yahoo.com>
2021-08-03 15:15:28 -0500

    repush since forgot to save files before pushing

---
+++ src/components/App.jsx
@@ -0,0 +1,3 @@
+import React, {Component} from 'react';
+import CreateFlashcards from './FlashcardCreator/flashcardCreator';
+import axios from 'axios';
--- src/components/FlashcardCreator/flashcardCreator.jsx
+++ src/components/FlashcardCreator/flashcardCreator.jsx
@@ -4,4 +4,52 @@ import React from 'react';
 
 const CreateFlashCards = (props) => {
     const { values, handleChange, handleSubmit } = 
+
+
+function create(){
+    props.addNewFlashcard(values);
+  }
+
+return ( 
+  <React.Fragment>
+    <form onSubmit={handleSubmit}>
+      <label>
+          Question:
+          <input 
+              type="text"
+              name="question"
+              onChange={handleChange}
+              values={values.question}
+              required={true}
+            />
+      </label>
+      <label>
+      Answer:
+              <input 
+                  type="text"
+                  name="answer"
+                  onChange={handleChange}
+                  values={values.answer}
+                  required={true}
+                />
+        </label>
+        <label>
+            Collection:
+                    <select name="collectionId" onChange={handleChange} values={values.id}>
+                        {props.collections.map((collection, id) => {
+                            <option key={id}>{id}</option>
+                            return (
+                                <option value={id+1}>
+                                    {collection.name}
+                                </option>
+                            )
+                        })}
+                    </select>
+        </label>
+        <input type="submit" className="btn btn-info" value="Add" />
+      </form>
+    </React.Fragment>
+   );
 }
+ 
+export default CreateFlashCards;
