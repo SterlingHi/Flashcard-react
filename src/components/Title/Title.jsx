940a281d3ad0d83fd3c246781f0ed6da86d4555c
coreyapat <coreyapat19@gmail.com>
2021-08-03 19:36:58 -0500

    Created a Title for the table and file

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Title = (props) => {
+    return (
+        <React.Fragment>
+            <div className="row row spacer">
+                <h1>dCC FlashCards</h1>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Title;
