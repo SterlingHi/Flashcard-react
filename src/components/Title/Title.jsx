772a19ff33360f8b325177a75adf8e269e4f11df
coreyapat <coreyapat19@gmail.com>
2021-09-01 07:23:18 -0500

    changed my funtion to a const with => within,

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import './title.css';
+import React from "react";
+import "./title.css";
 
-function Title(props) {
-    return (
-        <div className="row row-spacer">
-            <div className="col-md-12" style={{padding: 0}}>
-                <div className="titlebar col-align">                
-                    <h1>Flashcards</h1>
-                </div>
-            </div>
+const Title = (props) => {
+  return (
+    <div className="title">
+      <div className="col-md-12" style={{ padding: 0 }}>
+        <div className="title-nav">
+          <h1>Flashcards</h1>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Title;
