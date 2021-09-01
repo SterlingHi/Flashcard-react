ff46ff871cf7c6f7d3836d57f0d71e413f4b99e9
coreyapat <coreyapat19@gmail.com>
2021-09-01 07:06:02 -0500

    Fixed some minor issues within the reactFragment, and added a div and h1 tag.

--- src/components/displayAllCollections/displayAllCollections.jsx
+++ src/components/displayAllCollections/displayAllCollections.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-
+import React from "react";
+import "./displayAllCollections.css";
 const DisplayAllCollections = (props) => {
-    return (
-        <React.Fragment>
-            <table>
-                <tbody>
-                    <tr><td>Collections</td></tr>
-                        {props.collections.map((collections, id) => {
-                            return (
-                                <tr key= {id}>
-                                    <td>{collections.name}</td>
-                                    <td className="d-flex justify-content-center">
-                                        <button classname="btn btn-primary w-md-25" onClick={() => props.gatherFlashCards(collections.id)}>FlashCards</button>
-                                    </td>
-                                </tr>
-                         )
-                        })}
-                </tbody>
-            </table>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <div className="collections">
+        <h1>Available Collections:</h1>
+        <div>
+          {props.collections.map((collection, id) => {
+            return (
+              <div className="info" key={id}>
+                <h2>{collection.name}</h2>
+                <button
+                  className="btn btn-primary w-md-25"
+                  onClick={() => props.setShowFlashcards(collection)}
+                >
+                  All Flashcards
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default DisplayAllCollections;
